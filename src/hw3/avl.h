0c5601ae75fbdbd98ea10ced770189ab0a01b255
anna shalahina <anoshaosh@gmail.com>
2026-03-21 23:21:20 +0300

    add files hw3

---
+++ src/hw3/avl.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <stdio.h>
+
+typedef struct Node Node;
+
+Node* createNode(char* code, char* name);
+/*малое левое вращение*/
+Node* rotateLeft(Node* a);
+/*малое правое вращение*/
+Node* rotateRight(Node* a);
+/*большое правое вращение*/
+Node* bigRotateRight(Node* node);
+/*большое левое вращение*/
+Node* bigRotateLeft(Node* node);
+/*востанавливает баланс узла*/
+Node* balance(Node* node);
+/*вставка нового элемента*/
+Node* insert(Node* node, char* code, char* name);
+/*поиск минимального элемента*/
+Node* findMin(Node* node);
+/*удаление узла по коду*/
+Node* delete (Node* node, char* code);
+/*поиск узла*/
+Node* find(Node* node, char* code);
+/*возвращает название аэропорта по коду*/
+char* getName(Node* root, char* code);
+/*загрузка данных в файл*/
+Node* loadFromFile(char* filename, Node* root, int* count);
+/*обход дерева*/
+void inorderPrint(Node* node, FILE* fout);
+/*сохраняет и записывает дерево в файл*/
+void saveToFile(Node* root, char* filename);
+/*освобождение памяти всего дерева*/
+void freeTree(Node* node);
